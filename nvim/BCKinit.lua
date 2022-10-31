4517832f74555ac71f0aa88b8e707e4164407945
INV-matt <93540623+INV-matt@users.noreply.github.com>
2022-10-31 09:00:56 +0100

    Add files via upload

---
+++ nvim/BCKinit.lua
@@ -0,0 +1,111 @@
+local A = vim.api
+
+local function map(m, k, v)
+    vim.keymap.set(m, k, v, { silent = true })
+end
+
+--require('plugins')
+
+--	settings	--
+local g = vim.g
+local o = vim.o
+
+-- cmd('syntax on')
+-- vim.api.nvim_command('filetype plugin indent on')
+
+o.termguicolors = true
+-- o.background = 'dark'
+
+-- Do not save when switching buffers
+-- o.hidden = true
+
+-- Decrease update time
+o.timeoutlen = 500
+o.updatetime = 200
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 8
+
+-- Better editor UI
+o.number = true
+o.numberwidth = 3
+o.relativenumber = false --true
+o.signcolumn = 'no' --yes
+o.cursorline = true
+
+-- Better editing experience
+o.expandtab = true
+-- o.smarttab = true
+o.cindent = true
+o.autoindent = true
+o.wrap = true
+o.textwidth = 300
+o.tabstop = 4
+o.shiftwidth = 0
+o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.list = true
+o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
+-- o.listchars = 'eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ,'
+-- o.formatoptions = 'qrn1'
+
+-- Makes neovim and host OS clipboard play nicely with each other
+o.clipboard = 'unnamedplus'
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+-- o.backupdir = '/tmp/'
+-- o.directory = '/tmp/'
+-- o.undodir = '/tmp/'
+
+-- Remember 50 items in commandline history
+o.history = 50
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+-- Preserve view while jumping
+o.jumpoptions = 'view'
+
+-- BUG: this won't update the search count after pressing `n` or `N`
+-- When running macros and regexes on a large file, lazy redraw tells neovim/vim not to draw the screen
+-- o.lazyredraw = true
+
+-- Better folds (don't fold by default)
+-- o.foldmethod = 'indent'
+-- o.foldlevelstart = 99
+-- o.foldnestmax = 3
+-- o.foldminlines = 1
+
+-- Map <leader> to space
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+------------------------------------------
+
+A.nvim_create_autocmd('TextYankPost', {
+    group = num_au,
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'Visual', timeout = 120 })
+    end,
+})
+
+------------------------------------------
+
+-- Copying the vscode behaviour of making tab splits
+map('n', '<C-\\>', ':vsplit<CR>')
+map('n', '<A-\\>', ':split<CR>')
+
+------------------------------------------
+
+function _G.dump(...)
+    local objects = vim.tbl_map(vim.inspect, { ... })
+    print(unpack(objects))
+end
---
+++ nvim/plugin/packer_compiled.lua
@@ -0,0 +1,207 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+  local time
+  local profile_info
+  local should_profile = false
+  if should_profile then
+    local hrtime = vim.loop.hrtime
+    profile_info = {}
+    time = function(chunk, start)
+      if start then
+        profile_info[chunk] = hrtime()
+      else
+        profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+      end
+    end
+  else
+    time = function(chunk, start) end
+  end
+  
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+
+  _G._packer = _G._packer or {}
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/home/matteo/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/matteo/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/matteo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/matteo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/matteo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  catppuccin = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/catppuccin",
+    url = "https://github.com/catppuccin/nvim"
+  },
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
+  },
+  ["cmp-cmdline"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    url = "https://github.com/hrsh7th/cmp-cmdline"
+  },
+  ["cmp-nvim-lsp"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["cmp-nvim-lua"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    url = "https://github.com/hrsh7th/cmp-nvim-lua"
+  },
+  ["cmp-path"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/cmp-path",
+    url = "https://github.com/hrsh7th/cmp-path"
+  },
+  cmp_luasnip = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    url = "https://github.com/saadparwaiz1/cmp_luasnip"
+  },
+  ["friendly-snippets"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    url = "https://github.com/rafamadriz/friendly-snippets"
+  },
+  ["i3-vim-syntax"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/i3-vim-syntax",
+    url = "https://github.com/PotatoesMaster/i3-vim-syntax"
+  },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
+  ["nvim-autopairs"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
+  },
+  ["nvim-lsp-installer"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    url = "https://github.com/williamboman/nvim-lsp-installer"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-rooter.lua"] = {
+    config = { "\27LJ\2\n9\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\16nvim-rooter\frequire\0" },
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-rooter.lua",
+    url = "https://github.com/notjedi/nvim-rooter.lua"
+  },
+  ["nvim-tree.lua"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/kyazdani42/nvim-tree.lua"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["python-syntax"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/python-syntax",
+    url = "https://github.com/vim-python/python-syntax"
+  },
+  ["sxhkd-vim"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/sxhkd-vim",
+    url = "https://github.com/kovetskiy/sxhkd-vim"
+  },
+  ["vim-css-color"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/vim-css-color",
+    url = "https://github.com/ap/vim-css-color"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/home/matteo/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Config for: nvim-rooter.lua
+time([[Config for nvim-rooter.lua]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\16nvim-rooter\frequire\0", "config", "nvim-rooter.lua")
+time([[Config for nvim-rooter.lua]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
